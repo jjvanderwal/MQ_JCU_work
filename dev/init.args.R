ee2b6afb86042f02a14d3f00edca8e5aeb658f54
Jeremy VanDerWal <jjvanderwal@gmail.com>
2013-04-16 08:54:22 +1000

    initial setup of arguments

---
+++ dev/init.args.R
@@ -0,0 +1,12 @@
+#initial arguments used to define the inputs, models, outputs, etc
+
+wd <- "/home/jc140298/bccvl/" 	#define the core working directory
+species <- "ABT"	#define the species of interest
+occur.data <- "/home/jc165798/working/AWT.NERP/future.SDM/models/ABT/occur.csv"	#define the lat/lon of the observation records
+bkgd.data <- "/home/jc165798/working/AWT.NERP/future.SDM/models/ABT/bkgd.csv"	#define the lat/lon of the background / psuedo absence points to use
+
+enviro.data <- c()	#define the enviro data to use
+enviro.data.names <- c()	#define the names of the enviro data
+enviro.data.type <- c()	#type in terms of continuous or categorical
+
+
