016864af0e3d22aaa7d93199d1072c776f1a4831
erinmgraham <erin.graham@my.jcu.edu.au>
2013-04-18 16:03:45 +1000

    updates to match 01.model.current.R git push

--- dev/02.init.args.model.project.R
+++ dev/02.init.args.model.project.R
@@ -1,12 +1,26 @@
 #initial arguments used to define the inputs, models, outputs, etc
 
-wd <- "/home/jc140298/bccvl/" 	#define the core working directory
+#wd <- "/home/jc165798/working/BCCVL/models/ABT/" 	#define the core working directory
+wd <- "/home/jc140298/ABT/" 
 species <- "ABT"	#define the species of interest
-occur.data <- "/home/jc165798/working/AWT.NERP/future.SDM/models/ABT/occur.csv"	#define the lat/lon of the observation records
-bkgd.data <- "/home/jc165798/working/AWT.NERP/future.SDM/models/ABT/bkgd.csv"	#define the lat/lon of the background / psuedo absence points to use
 
-enviro.data <- c()	#define the enviro data to use
-enviro.data.names <- c()	#define the names of the enviro data
-enviro.data.type <- c()	#type in terms of continuous or categorical
+enviro.data.dir <- "/home/jc165798/working/BCCVL/envirodata"	#define the enviro data directory to use
+enviro.data.names <- c("bioclim_01","bioclim_04","bioclim_05","bioclim_06",
+	"bioclim_12","bioclim_15","bioclim_16","bioclim_17") #define the names of the enviro data
+	# EMG Not sure if this is needed, might use to check layers prior to predicting DOMAIN
 
+### define the models to be used for projection
+project.bioclim = TRUE #boolean to project BIOCLIM algorithm 
+project.domain = TRUE #boolean to project DOMAIN algorithm -- envirodata should have same number of data layers as model creation
+project.mahal = TRUE #boolean to project MAHALANOBIS algorithm
+project.geodist = TRUE #boolean to project geographic distances algorithm
+project.convHull = TRUE #boolean to project convex hulls algorithm 
+project.circles = TRUE #boolean to project circles algorithm 
+project.geoIDW = TRUE #boolean to project inverse distance weighted algorithm
+project.voronoiHull = TRUE #boolean to project Voronoi Hulls algorithm
+project.brt = TRUE #boolean to project Boosted regression tree algorithm
 
+project.maxent = FALSE #boolean to project maxent algorithm
+if (project.maxent) {
+	maxent.jar = "/home/jc165798/working/BCCVL/maxent.jar" #define location of maxent.jar file
+}
--- dev/02.model.project.R
+++ dev/02.model.project.R
@@ -1,81 +1,150 @@
+#script to project distribution models
 
-# Date Created: 16 April 2013
-# Last Modified By: EGraham 16.04.13
+#source("/home/jc165798/SCRIPTS/git_code/MQ_JCU_work/dev/02.init.args.model.project.R") #read in the initial arguments
+source("/home/jc140298/bccvl/02.init.args.model.project.R")
 
-# This script will project species' distributions using the dismo package
-# Requires SDM fits
-# BIOCLIM, DOMAIN, MAHAL, MAXENT, BRT, GEODIST, CONVEXHULL, CIRCLES, GEOIDW, VORONOIHULL
+### check if libraries are installed, install if necessary and then load them
+necessary=c("dismo","SDMTools","gbm","gstat","deldir") #list the libraries needed
+installed = necessary %in% installed.packages() #check if library is installed
+if (length(necessary[!installed]) >=1) install.packages(necessary[!installed], dep = T) #if library is not installed, install it
+for (lib in necessary) library(lib,character.only=T)#load the libraries
 
-# source("02.model.project.R")
 
-# start with a clean environment
-rm(list=ls())
-
-# load libraries required
-library(dismo)
-library(SDMTools)	# for read.as.gz()
-library(gbm)
-
-# get initial arguments for projections - will need to modify this
-#source("02.init.args.model.project.R")
-
-# set arguments for now
-species="ABT"
-models = c("BIOCLIM", "DOMAIN", "MAHAL", "BRT", "GEODIST", "CONVEXHULL", "CIRCLES", 
-	"GEOIDW", "VORONOIHULL", "MAXENT")
-enviro.dir = "/home/jc165798/working/BCCVL/envirodata"
-
+### read in the environmental data
+# get list of directories, each directory should have env files related to a climate scenario
+scenarios = list.files(enviro.data.dir)
 # create a list to hold the climate scenario names and RasterStack of data layers
 predictors = list()
-# get list of directories, each directory should have env files related to a climate scenario
-scenarios = list.files(enviro.dir)
-# create RasterStacks of enviro data
+# combine predictors into a RasterStack of enviro data
 for (f in 1:length(scenarios)) {
-	scenario.files = list.files(paste(enviro.dir, scenarios[f], sep="/"), full.names=TRUE)
+	scenario.files = list.files(paste(enviro.data.dir, scenarios[f], sep="/"), full.names=TRUE)
 	scenario.stack = stack(scenario.files)
 	predictors[[f]] = list(scenarios[f], scenario.stack)
 } # end for scenarios
 
-# for each algorithm
-for (m in models) {
+# function to get model object
+getModelObject = function(model.name) {
 
-	# get the model object
-	# where model outputs are stored until jeremy's script is run
-	datadir = paste("/home/jc140298/", species, "/output_", m, "/", sep="")
-	obj.name = paste(m, "obj.sdm", sep="_")
-	hold = load(file=paste(datadir, sep="", obj.name))
-	proj.model = get(hold)
+	model.dir = paste(wd, "output_", model.name, "/", sep="")
+	model.obj = get(load(file=paste(model.dir, "model.object.RData", sep="")))
+	return (model.obj)
+}
+
+# function to save project output
+saveModelProjection = function(out.model, model.name, proj.name) {
 	
-	# for each set of predictors 
-	for (s in 1:length(predictors)) {
+	model.dir = paste(wd, "output_", model.name, "/", sep="")
+	outdir = paste(model.dir, "project_", proj.name, sep=""); dir.create(outdir,recursive=TRUE);
+	writeRaster(out.model, paste(outdir, proj.name, sep="/"), format="GTiff")
+}
 
-		if (!(m %in% c("MAXENT", "BRT"))) {
-		
-			# have to drop layers for DOMAIN if they weren't in original model
-			if (m == "DOMAIN") {
-				predictors.dm = dropLayer(predictors[[s]][[2]], c("bioclim_02","bioclim_03","bioclim_07",
-					"bioclim_08","bioclim_09","bioclim_10","bioclim_11","bioclim_13","bioclim_14",
-					"bioclim_18","bioclim_19"))
-				project = predict(proj.model, predictors.dm)
-			} else {
-				# make projection
-				projection = predict(proj.model, predictors[[s]][[2]])
-			} # end if domain
-			
-		}  else if (m == "BRT") {
-		
-			# make projection
-			projection = predict(predictors[[s]][[2]], proj.model, n.trees=proj.model$gbm.call$best.trees)
-				
-		} else if (m == "MAXENT") {
-			cat("Not implemented")
-		} # end if
-		
-		# save output
-		outfilename = paste(datadir, "project_", predictors[[s]][[1]], sep="")
-		writeRaster(projection, outfilename, format="ascii") 
-#			format="GTiff", 
-#			options=c("PHOTOMETRIC=CMYK", "JPEG_QUALITY=75", "COMPRESS=LZW"),
-#			overwrite=TRUE)
-	} # end for predictors
-} # end for models
+# for each set of predictors 
+for (s in 1:length(predictors)) {
+	
+if (project.bioclim) {
+
+	bioclim.obj = getModelObject("bioclim")	# get the model object
+	bioclim.proj = predict(bioclim.obj, predictors[[s]][[2]])	# predict for given climate scenario
+	saveModelProjection(bioclim.proj, "bioclim", predictors[[s]][[1]])	# save output
+	rm(list=c("bioclim.obj", "bioclim.proj")); #clean up the memory
+}
+	
+if (project.domain) {
+
+	domain.obj = getModelObject("domain") # get the model object
+	# have to drop env layers for DOMAIN if they weren't in original model
+	# get the names of the env layers used to create DOMAIN model
+	domain.layers = colnames(domain.obj@presence)	# should be the same as 'enviro.data.names'
+	# get the names of the climater scenario's env layers
+	pred.layers = names(predictors[[s]][[2]])
+
+	# create a new list of env predictors by dropping layers not in the original model
+	predictors.dm = predictors[[s]][[2]]
+	for (pl in pred.layers) {
+		if (!(pl %in% domain.layers)) {
+			predictors.dm = dropLayer(predictors.dm, pl)
+		}	
+	}
+
+	domain.proj = predict(domain.obj, predictors.dm) # predict for given climate scenario
+	saveModelProjection(domain.proj, "domain", predictors[[s]][[1]]) 	# save output
+	rm(list=c("domain.obj", "domain.proj")); #clean up the memory
+}
+
+if (project.mahal) {
+	
+	mahal.obj = getModelObject("mahal") # get the model object
+	mahal.proj = predict(mahal.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(mahal.proj, "mahal", predictors[[s]][[1]]) 	# save output
+	rm(list=c("mahal.obj", "mahal.proj")); #clean up the memory
+}
+
+if (project.geodist) {
+	
+	geodist.obj = getModelObject("geodist") # get the model object
+	geodist.proj = predict(geodist.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(geodist.proj, "geodist", predictors[[s]][[1]]) 	# save output
+	rm(list=c("geodist.obj", "geodist.proj")); #clean up the memory
+}
+
+if (project.convHull) {
+	
+	convHull.obj = getModelObject("convHull") # get the model object
+	convHull.proj = predict(convHull.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(convHull.proj, "convHull", predictors[[s]][[1]]) 	# save output
+	rm(list=c("convHull.obj", "convHull.proj")); #clean up the memory
+}
+
+if (project.circles) {
+	
+	circles.obj = getModelObject("circles") # get the model object
+	circles.proj = predict(circles.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(circles.proj, "circles", predictors[[s]][[1]]) 	# save output
+	rm(list=c("circles.obj", "circles.proj")); #clean up the memory
+}
+
+if (project.geoIDW) {
+	
+	geoIDW.obj = getModelObject("geoIDW") # get the model object
+	geoIDW.proj = predict(geoIDW.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(geoIDW.proj, "geoIDW", predictors[[s]][[1]]) 	# save output
+	rm(list=c("geoIDW.obj", "geoIDW.proj")); #clean up the memory
+}
+
+if (project.voronoiHull) {
+	
+	voronoiHull.obj = getModelObject("voronoiHull") # get the model object
+	voronoiHull.proj = predict(voronoiHull.obj, predictors[[s]][[2]]) # predict for given climate scenario
+	saveModelProjection(voronoiHull.proj, "voronoiHull", predictors[[s]][[1]]) 	# save output
+	rm(list=c("voronoiHull.obj", "voronoiHull.proj")); #clean up the memory
+}
+
+if (project.brt) {
+	
+	brt.obj = getModelObject("brt") # get the model object
+	# NOTE the order of arguments in the  predict function for brt; this is because
+	#	the function is defined outside of the dismo package
+	brt.proj = predict(predictors[[s]][[2]], brt.obj, n.trees=brt.obj$gbm.call$best.trees) # predict for given climate scenario
+	saveModelProjection(brt.proj, "brt", predictors[[s]][[1]]) 	# save output
+	rm(list=c("brt.obj", "brt.proj")); #clean up the memory
+}
+
+#if (project.maxent) {
+
+	#*************** UNDER CONSTRUCTION ***************
+	# maxent model creation was run as a system call outside of R, need to do the same for projection
+	# EMG check to see if argument defaults / modifiables are the same as during creation
+
+	### not user modified section
+#	tstr = ("module load java\n")
+#	tstr = paste(tstr, "java -cp ", maxent.jar, " density.Project ", wd, "output_maxent/ABT.lambdas ", sep="")
+	# where to find the climate scenarios
+#	tstr = paste(tstr, enviro.data.dir, "/", scenarios[s], " ", sep="")
+	# where to put the output
+#	tstr = paste(tstr, wd, "output_maxent/", scenarios[s], "/projection.asc", sep="")
+	# optional arguments
+#	tstr = paste(tstr, " nowriteclampgrid nowritemess fadebyclamping", sep="")
+#	system(tstr)
+#}
+
+} # end for predictors
