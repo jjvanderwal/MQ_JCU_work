fed2bc5ea8166282ef21e766bf0810d9dc0e7c4e
erinmgraham <erin.graham@my.jcu.edu.au>
2013-11-04 18:24:04 +1000

    modified for new directory strucutre, still incomplete

--- separate_scripts/01.shellscript.model.R
+++ separate_scripts/01.shellscript.model.R
@@ -1,49 +1,68 @@
 # create the shell script to set the arguments and run the models for each species
 
-# get a list of species directories
-species.names = list.files("/home/jc165798/working/BCCVL/models") #get a list of all the species
+# define working dir
+wd = "/rdsi/ccimpacts/SDM_assessment"
+
+# define taxa
+taxa = c("mammals", "birds", "reptiles", "amphibians")
+
+# define spatial scales
+scales = c("5km", "1km", "250m")
 
 # create a list of model algorithms
 model.algorithms = c("bioclim", "domain", "mahal", "geodist", "convHull", "circles", "geoIDW", "voronoiHull", "brt", "maxent",
 	"glm", "gam", "gbm", "cta", "ann", "sre", "fda", "mars", "rf", "biomod.maxent")
 
-# define working directory
-wd = "/home/jc140298/ibccvl"
+script.dir = "https://github.com/jjvanderwal/MQ_JCU_work/blob/master/separate_scripts"
 
-for (sp in species.names) { #cycle through each of the species
+for (taxon in taxa) {
 
-	# set the species arg
-	species.arg = sp	
-	# set the location of the occurrence data
-	occur.data.arg = paste("/home/jc165798/working/BCCVL/models/", sp, "/occur.csv", sep="") 
-	# set the location of the background data
-	bkgd.data.arg = paste("/home/jc165798/working/BCCVL/models/", sp, "/bkgd.csv", sep="") 
+	taxon.dir = paste(wd, "/", taxon, "_emg", sep="")
 
-	# create the species specific working directory argument
-	sp.wd.arg = paste(wd, "/", sp, sep=""); dir.create(sp.wd.arg);	setwd(sp.wd.arg) 
+	for (i in 1:length(scales[1])) {
+	#EMG Only 5km is currently available (in current.76to05)
 
-	for (model in model.algorithms) { # cycle through each model algorithm
+		# set the location of the background data
+		bkgd.data.arg = paste(taxon.dir, "/", scales[i], "_bkgd.csv", sep="")
+
+		# get a list of species directories
+		species.names = list.files(paste(taxon.dir, "/models", sep="")) #get a list of all the species
+
+		for (sp in species.names[1:2]) { #cycle through each of the species
+		
+			# create the species specific working directory argument
+			sp.wd.arg = paste(taxon.dir, "/models/", sp, "/", scales[i], sep=""); setwd(sp.wd.arg) 
+
+			# set the species arg
+			species.arg = sp	
+			# set the location of the occurrence data
+			occur.data.arg = paste(sp.wd.arg, "/occur.csv", sep="") 
+
+			for (model in model.algorithms) { # cycle through each model algorithm
 	
-		# create output directory
-		outdir = paste(sp.wd.arg, "/output_", model, sep=''); dir.create(outdir,recursive=TRUE);
+				# create output directory
+				outdir = paste(sp.wd.arg, "/output_", model, sep=''); dir.create(outdir,recursive=TRUE);
 		
-		# create the shell file
-		shell.file.name = paste(outdir, "/01.", model, ".model.", sp, ".sh", sep="")
-		
-		shell.file = file(shell.file.name, "w")
-			cat('#!/bin/bash\n', file=shell.file)
-			cat('#PBS -j oe\n', file=shell.file) # combine stdout and stderr into one file
-			cat('cd $PBS_O_WORKDIR\n', file=shell.file)
-			cat('source /etc/profile.d/modules.sh\n', file=shell.file) # need for java
-			cat('module load java\n', file=shell.file) # need for maxent
-			cat('module load R\n', file=shell.file) # need for R
-			# this job calls the 01.init.args.model.R file using arguments defined above to set the parameters for the models
-			cat("R CMD BATCH --no-save --no-restore '--args wd=\"", sp.wd.arg, "\" species=\"", species.arg, "\" occur.data=\"", occur.data.arg, "\" bkgd.data=\"", bkgd.data.arg, "\"' ", wd, "/01.init.args.model.R ", outdir, "/01.init.args.model.", sp, ".Rout \n", sep="", file=shell.file)
-			# this job calls the 01.model.R file to run the models
-			cat("R CMD BATCH --no-save --no-restore '--args wd=\"", sp.wd.arg, "\" species=\"", species.arg, "\"' ", wd, "/01.", model, ".model.R ", outdir, "/01.", model, ".model.", sp, ".Rout \n", sep="", file=shell.file)
-		close(shell.file)
+				# create the shell file
+				shell.file.name = paste(outdir, "/01.", model, ".model.", sp, ".sh", sep="")
+			
+				shell.file = file(shell.file.name, "w")
+					cat('#!/bin/bash\n', file=shell.file)
+					cat('#PBS -j oe\n', file=shell.file) # combine stdout and stderr into one file
+					cat('cd $PBS_O_WORKDIR\n', file=shell.file)
+					cat('source /etc/profile.d/modules.sh\n', file=shell.file) # need for java
+					cat('module load java\n', file=shell.file) # need for maxent
+					cat('module load R\n', file=shell.file) # need for R
+					# this job calls the 01.init.args.model.R file using arguments defined above to set the parameters for the models
+					cat("R CMD BATCH --no-save --no-restore '--args wd=\"", sp.wd.arg, "\" species=\"", species.arg, "\" occur.data=\"", occur.data.arg, "\" bkgd.data=\"", bkgd.data.arg, "\"' ", script.dir, "/01.init.args.model.R ", outdir, "/01.init.args.model.", sp, ".Rout \n", sep="", file=shell.file)
+					# this job calls the 01.model.R file to run the models
+					cat("R CMD BATCH --no-save --no-restore '--args wd=\"", sp.wd.arg, "\" species=\"", species.arg, "\"' ", script.dir, "/01.", model, ".model.R ", outdir, "/01.", model, ".model.", sp, ".Rout \n", sep="", file=shell.file)
+				close(shell.file)
 
-		# submit job
-		system(paste("qsub -l nodes=1:ppn=1 -l pmem=2gb ", shell.file.name, sep=""))
-	} # end for model
-} # end for species
+				# submit job
+				system(paste("qsub -l nodes=1:ppn=1 -l pmem=2gb ", shell.file.name, sep=""))
+			} # end for model
+		} # end for species
+	} # end for scales
+} # end for taxa
+
