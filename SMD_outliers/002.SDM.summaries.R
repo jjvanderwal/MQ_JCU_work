1b2c48b6977e8fda5f4205c85352f68d74418750
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-07-07 14:53:22 +1000

    started summarizing for ms

---
+++ SMD_outliers/002.SDM.summaries.R
@@ -0,0 +1,4 @@
+#drafted by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
+#GNU General Public License .. feel free to use / distribute ... no warranties
+
+################################################################################
