41f2744035c2e74915b170ee5225ff857efe50af
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-07-09 13:33:11 +1000

    have summaries for current runing with minor bug fixes

--- SMD_outliers/002.script2run.R
+++ SMD_outliers/002.script2run.R
@@ -8,7 +8,7 @@ args=(commandArgs(TRUE)) #get the command line arguements
 for(i in 1:length(args)) { eval(parse(text=args[[i]])) } #evaluate the arguments
 #should have read in spp
 print(spp)
-# spp = 'Acrobates_pygmaeus'
+# spp = 'Antechinus_leo'
 
 
 ###############################################################################
@@ -58,16 +58,18 @@ if (length(grep('output_',colnames(out))>0)) colnames(out) = gsub('output_','',c
 occur = read.csv(paste(data.dir,spp,'/5km/occur.csv',sep=''),as.is=TRUE) #read in teh occur
 occur$pa = 1; occur = occur[,c('lon','lat','pa')]
 out = merge(out,occur,all=TRUE); out$pa[which(is.na(out$pa))] = 0 
+n.obs = nrow(occur) #define the number of obs
 
 ###do any scaling and calculate thresholds & do class stats...
 sum.out = NULL
 for (coi in Algs) { cat('\t',coi,'\n')
 	tdata = out[,coi]
-	if (all(is.na(tdata)) & !coi %in% Algs.geo ) {
+	if ((all(is.na(tdata)) | sum(tdata)==0) & !coi %in% Algs.geo ) {
 		if (is.null(sum.out)) {
 			sum.out = NULL
 		} else {
-			sum.out = as.data.frame(rbind(sum.out,c(spp,coi,rep(NA,ncol(sum.out)-2))),stringsAsFactors=FALSE)
+			tout = sum.out[1,]; tout[1,] = c(spp,coi,n.obs,rep(NA,ncol(sum.out)-2))
+			sum.out = rbind(sum.out,tout)
 		}
 	} else if (coi %in% Algs.geo) {
 			out[,coi] = NULL #remove from dataset	
@@ -75,21 +77,25 @@ for (coi in Algs) { cat('\t',coi,'\n')
 		if (coi %in% Algs.biomod) {
 			tdata = tdata/1000
 			tt = optim.thresh(out$pa,tdata,101); tt = as.data.frame(tt)[1,]; colnames(tt) = paste('threshold.',colnames(tt),sep='') #kappa.fun(out$pa,tdata)
-			tout = cbind(data.frame(spp=spp,alg=coi,raw.threshold=tt$threshold.max.sensitivity.specificity[1]*1000,as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
+			tt$maxKappa = accuracy(out$pa,tdata,tt$threshold.maxKappa[1])$Kappa; tt$AUC.full = auc(out$pa,tdata)
+			tout = cbind(data.frame(spp=spp,alg=coi,n.obs=n.obs,raw.threshold=tt$threshold.max.sensitivity.specificity[1]*1000,as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
 		} else if (coi %in% c("brt","mahal")) {
 			tdata = (out[,coi] + abs(min(out[,coi],na.rm=TRUE)))/diff(range(out[,coi],na.rm=TRUE))
 			tt = optim.thresh(out$pa,tdata,101); tt = as.data.frame(tt)[1,]; colnames(tt) = paste('threshold.',colnames(tt),sep='') #kappa.fun(out$pa,tdata)
-			tout = cbind(data.frame(spp=spp,alg=coi,raw.threshold=tt$threshold.max.sensitivity.specificity[1]*diff(range(out[,coi],na.rm=TRUE))-abs(min(out[,coi],na.rm=TRUE)),as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
+			tt$maxKappa = accuracy(out$pa,tdata,tt$threshold.maxKappa[1])$Kappa; tt$AUC.full = auc(out$pa,tdata)
+			tout = cbind(data.frame(spp=spp,alg=coi,n.obs=n.obs,raw.threshold=tt$threshold.max.sensitivity.specificity[1]*diff(range(out[,coi],na.rm=TRUE))-abs(min(out[,coi],na.rm=TRUE)),as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
 		} else {
 			tt = optim.thresh(out$pa,tdata,101); tt = as.data.frame(tt)[1,]; colnames(tt) = paste('threshold.',colnames(tt),sep='') #kappa.fun(out$pa,tdata)
-			tout = cbind(data.frame(spp=spp,alg=coi,raw.threshold=tt$threshold.max.sensitivity.specificity[1],as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
+			tt$maxKappa = accuracy(out$pa,tdata,tt$threshold.maxKappa[1])$Kappa; tt$AUC.full = auc(out$pa,tdata)
+			tout = cbind(data.frame(spp=spp,alg=coi,n.obs=n.obs,raw.threshold=tt$threshold.max.sensitivity.specificity[1],as.data.frame(tt)[1,]),accuracy(out$pa,tdata,tt$threshold.max.sensitivity.specificity[1]))
 		}
 		tdata[which(tdata<tt$threshold.max.sensitivity.specificity[1])] = 0; tdata[which(tdata>0)] = 1 #apply the thresholds
 		tasc = baseasc; tasc[cbind(pos$row,pos$col)] = tdata
 		tt2 = ClassStat(tasc,latlon=TRUE); tt2 = tt2[which(tt2$class==1),]
 		tout = cbind(tout,tt2)
-		sum.out = as.data.frame(rbind(sum.out,tout),stringsAsFactors=FALSE) #record the info out
+		sum.out = rbind(sum.out,tout) #record the info out
 	}
+	sum.out$alg = as.character(sum.out$alg)
 }
 write.csv(sum.out,paste(spp,'.summaries.csv',sep=''),row.names=FALSE) #write out the thresholds
 
